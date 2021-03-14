6153bfd680cd143e81870affbd3827459efb5be4
joshqwang <63423092+joshqwang@users.noreply.github.com>
2021-03-13 20:42:05 -0600

    Update bot.py

--- owshelper/bot.py
+++ owshelper/bot.py
@@ -8,72 +8,66 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
 contributors = {}
-current_story = ''
-ongoing_story = False
-current_channel = ''
 
-def reset_vars():
-    global ongoing_story
-    global current_story
-    global current_channel
-    
+all_stories = {}
+#{guild_name: (story, ongoing, channel, contributors)}
+
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 
 @client.event
 async def on_message(message):
-    global ongoing_story
-    global current_story
+    global all_stories
     global current_channel
     global contributors
     if message.author == client.user:
       return
-    
+    if not message.guild.name in all_stories:
+          all_stories[message.guild.name] = ('', False,'', {})
     if message.content == 'ows start':
-      if current_channel != '':
+      
+      if all_stories[message.guild.name][2] != '':
           await message.channel.send('Ending a story in other channel!')
 
-      current_story = ''
-      ongoing_story = True
-      current_channel = message.channel.name
+      all_stories[message.guild.name] = ('', True, message.channel.name, {})
       await message.channel.send('Starting a story!')
     elif message.content == 'ows current':
-     if ongoing_story == False:
+     if all_stories[message.guild.name][1] == False:
         await message.channel.send('There is not an ongoing story right now!')
-     elif current_story == '':
+     elif all_stories[message.guild.name][0] == '':
         await message.channel.send('The current story is empty.')
      else:
-        await message.channel.send('Your current story is: ' + current_story)
+        await message.channel.send('Your current story is: ' + all_stories[message.guild.name][0])
 
      
 
     elif message.content == 'ows end':
-      if ongoing_story == False:
+      if all_stories[message.guild.name][1] == False:
         await message.channel.send('There is not an ongoing story right now!')
-      elif current_story == '':
+      elif all_stories[message.guild.name][0] == '':
         await message.channel.send('Ended the story.')
       else:
-        contributors_items = list(contributors.items())
+        contributors_items = list(all_stories[message.guild.name][3].items())
         contributors_items.sort(key = lambda x : x[1], reverse = True)
         contributors_message = ''
         for key in contributors_items:
           contributors_message += key[0] + " - " + str(key[1]) + " contributions \n"
-        await message.channel.send('Your story is: ' + current_story +  '\n\nThe top contributors in this story were:\n' + contributors_message)
+        await message.channel.send('Your story is: ' + all_stories[message.guild.name][0] +  '\n\nThe top contributors in this story were:\n' + contributors_message)
 
-      current_story = ''
-      ongoing_story = False
-      current_channel = ''
-      contributors = {}
+      all_stories[message.guild.name] = ('', False, '', {})
 
-    elif ongoing_story == True and message.channel.name == current_channel:
-      current_story += message.content + ' '
-      if message.author.display_name in contributors:
-        contributors[message.author.display_name] += 1
+    elif all_stories[message.guild.name][1] == True and message.channel.name == all_stories[message.guild.name][2]:
+      all_stories[message.guild.name] = (all_stories[message.guild.name][0] + message.content + ' ', all_stories[message.guild.name][1], all_stories[message.guild.name][2], all_stories[message.guild.name][3])
+      if message.author.display_name in all_stories[message.guild.name][3]:
+        temp_dict = all_stories[message.guild.name][3]
+        temp_dict[message.author.display_name] += 1
+        all_stories[message.guild.name] = (all_stories[message.guild.name][0], all_stories[message.guild.name][1], all_stories[message.guild.name][2], temp_dict)
       else:
-        contributors[message.author.display_name] = 1
+        temp_dict = all_stories[message.guild.name][3]
+        temp_dict[message.author.display_name] = 1
+        all_stories[message.guild.name] = (all_stories[message.guild.name][0], all_stories[message.guild.name][1], all_stories[message.guild.name][2], temp_dict)
       await message.add_reaction('\N{THUMBS UP SIGN}')
 
-    	
 
 client.run(TOKEN)
